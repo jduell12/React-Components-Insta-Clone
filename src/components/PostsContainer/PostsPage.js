96a8e0c6bc3561e088f878de1024a1e901181f41
Jessica <duellje@oregonstate.edu>
2020-06-09 15:39:01 -0400

    added search bar to top of page. Passing data to post component

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,21 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
-// import data 
+import usersData from '../../dummy-data'
 
 const PostsPage = () => {
   // set up state for your data
+  const[users, setUser] = useState(usersData);
+  console.log(users);
   return (
+    
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+      {users.map(user => {
+        return <Post userData = {user}/>
+      })}
     </div>
   );
 };
