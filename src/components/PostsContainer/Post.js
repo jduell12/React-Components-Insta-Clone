77fb2734087c69ceea52a06f8a85f820c2df0ad4
Jessica <duellje@oregonstate.edu>
2021-02-14 21:09:13 -0500

    completed PostsPage

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,5 @@
 // You will add code in this file
-import React from "react";
+import React, { useState } from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -7,16 +7,14 @@ import PostHeader from "./PostHeader";
 import "./Posts.css";
 
 // pass props in this file to
-const Post = props => {
+const Post = (props) => {
   // set up state for the likes
-
+  const [likes, setLikes] = useState("");
   return (
     <div className="post-border">
       <PostHeader
         username={props.post.username}
-        thumbnailUrl={
-          props.post.thumbnailUrl
-        }
+        thumbnailUrl={props.post.thumbnailUrl}
       />
       <div className="post-image-wrapper">
         <img
